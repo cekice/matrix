38ee7491235a817828bebfa57edb4f33d693c02f
edin.cekic <cekic.edin@hotmail.com>
2018-12-10 12:53:23 +0100

    Commit

--- matrix/Program.cs
+++ matrix/Program.cs
@@ -10,12 +10,19 @@ namespace matrix
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string[][] users = {
+        new string[] { "Peter", "Smith", "1111" },
+        new string[] { "Sally", "Simmons", "2222" },
+        new string[] { "Ross", "Richards", "3333" }
+        };
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            string[] names = new string[users.Length];
+            for (int i = 0; i < users.Length; i++)
+                names[i] = users[i][0];
+            foreach (string name in names)
+                Console.WriteLine(name);
+            Console.Read();
         }
     }
+
 }
